6a7364ad23c2529ea702ee2aefb3d138581ad69c
dinukah <dinukahamarasinghe@gmail.com>
2023-10-03 09:10:11 +0530

    previous/next song button functionality

--- script.js
+++ script.js
@@ -37,12 +37,32 @@ function pauseSong() {
     audio.pause();
 }
 
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
-
     if (isPlaying) {
         pauseSong();
     } else {
         playSong();
     }
 });
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
